c5a8c57640a752333de07e394d0d6a834b42af96
Benzeman97 <nafazbenzema@gmail.com>
2026-01-15 17:49:10 +0530

    project initialized with all the required documents

---
+++ patient-management-service/src/test/java/com/monitoredrx/patient/PatientManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoredrx.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
