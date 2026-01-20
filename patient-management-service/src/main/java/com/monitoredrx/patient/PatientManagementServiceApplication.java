0363ab91ef0209aa660ef1a54d3457c2080856a8
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-20 18:07:29 +0530

    Update PatientManagementServiceApplication.java

--- patient-management-service/src/main/java/com/monitoredrx/patient/PatientManagementServiceApplication.java
+++ patient-management-service/src/main/java/com/monitoredrx/patient/PatientManagementServiceApplication.java
@@ -3,9 +3,11 @@ package com.monitoredrx.patient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class PatientManagementServiceApplication {
 
 	public static void main(String[] args) {
